7f18f7ed9ec86b4860f5d1f528f7a7ef4719b251
WyrwaMichal8 <kokojakikoko@gmail.com>
2025-10-14 10:01:14 +0000

    1_1.2

--- script.js
+++ script.js
@@ -11,5 +11,27 @@
     ex1_content.innerHTML = tabela.toString();
   };
 
+  document.getElementById("ex2_text").addEventListener("input", number);
+
+  function number() {
+
+    var number = document.getElementById("ex2_text");
+    var log = document.getElementById('ex2_content');
+
+
+    log.textContent = "Numer telefonu jest poprawny";
+
+    if (number.value.match(/[\W]/)) {
+      log.textContent = "Numer nie może zawierać znaków specjalnych";
+    }
+
+    if (number.value.match(/[a-zA-Z]/)) {
+      log.textContent = "Numer nie może zawierać liter";
+    }
+
+    if (number.value.lenght != 9) {
+      log.textContent = "Długość numeru musi być równa 9";
+    }
+  }
 
 })();
