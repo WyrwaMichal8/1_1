2cd36f536606a81649d17dfd1e1d11a5923a5658
WyrwaMichal8 <kokojakikoko@gmail.com>
2025-10-27 22:35:48 +0000

    1_2

--- script.js
+++ script.js
@@ -10,4 +10,31 @@
     }
     ex1_content.innerHTML = tabela.toString();
   };
+
+  document.getElementById("ex2_text").addEventListener("change", numer);
+
+  function numer() {
+
+    var number = document.getElementById("ex2_text");
+    var log = document.getElementById("ex2_content");
+
+    if (number.value.length != 9) {
+      log.textContent = "Długość numeru musi być równa 9 ";
+      //log.textContent = string(number.value.lenght);
+    }
+
+    else if (number.value.match(/[a-zA-Z]/)) {
+      log.textContent = "Numer nie może zawierać liter";
+    }
+
+    else if (number.value.match(/[\W]/)) {
+      log.textContent = "Numer nie może zawierać znaków specjalnych";
+    }
+
+    else {
+      log.textContent = "Numer telefonu jest poprawny";
+    }
+
+  }
+
 })();
